3a1c339590f2def1be0e0640522ec0a5a4c8d41b
DD <aizuddin.hamzah@timeteccloud.com>
2023-05-28 02:46:01 +0800

    connect to dummy api

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,32 +7,36 @@ import SMEInfoPieChart from "@/components/SMEInfoPieChart";
 import ResponseRateChart from "@/components/ResponseRateChart";
 import QrUsageChart from "@/components/QrUsageChart";
 import ReviewerList from "@/components/shared/ReviewerList";
-import { getData } from "@/db";
+import { getResponseRate } from "@/db";
 import { useEffect, useState } from "react";
 
 const Page = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getData();
+      const result = await getResponseRate();
       setData(result);
+      setLoading(false)
     };
     fetchData();
   }, []);
+
+  if (loading) {
+    return null
+  }
 
   return (
     <>
       <main className="bg-gray-100 min-h-screen">
         <Header />
-        <TopCards />
+        <TopCards data = {data}/>
         <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
           <ResponseRateChart />
-          <ResponseRateChart />
-          <ReviewerList />
-          <ChallengesBarChart />
-          <pre>
-            <code>{JSON.stringify(data, null, 2)}</code>
-          </pre>
+          <ResponseRateChart  />
+          <ReviewerList data = {data}/> 
+          <ChallengesBarChart data = {data} />
+        
         </div>
       </main>
     </>
--- src/components/shared/TopCards.js
+++ src/components/shared/TopCards.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-const TopCards = () => {
+const TopCards = ({data}) => {
+    
   return (
     <div className='grid lg:grid-cols-4 gap-4 p-4'>
         <div className='lg:col-span-1 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
             <div className='flex flex-col w-full pb-4'>
-                <p className='text-2xl font-bold'>300</p>
-                <p className='text-gray-600'>responded SME Percentage</p>
+                <p className='text-2xl font-bold'>{data.respondedSme}</p>
+                <p className='text-gray-600'>responded SME </p>
             </div>
             <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
-                <span className='text-green-700 text-lg'>18%</span>
+                <span className='text-green-700 text-lg'>{data.respondedSmePerc}</span>
             </p>
         </div>
         <div className='lg:col-span-1 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
