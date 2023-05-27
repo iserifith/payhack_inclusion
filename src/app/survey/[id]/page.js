b13ce47b2bea4deaf4bfeacc66ecaa8f9668f51a
xrn <seriputera2@gmail.com>
2023-05-28 00:49:12 +0800

    --

--- src/app/survey/[id]/page.js
+++ src/app/survey/[id]/page.js
@@ -1,211 +1,142 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
-import { Listbox, RadioGroup } from "@headlessui/react";
+import { useState } from "react";
 
-const questions = [
+const _questions = [
   {
-    text: "What is your name?",
-    type: "text",
-    required: true,
-    validation: () => {
-      return true;
-    },
-  },
-  {
-    text: "How old are you?",
-    type: "text",
-    required: true,
-    validation: () => {
-      return true;
-    },
-  },
-  {
-    text: "Which of the following fruits do you like?",
-    type: "choice",
-    required: true,
-    options: ["Apples", "Oranges", "Bananas", "Pears"],
-    validation: () => {
-      return true;
-    },
-  },
-  {
-    text: "Provide multiple answers",
-    type: "multiple_choices",
-    options: ["Apples", "Oranges", "Bananas", "Pears"],
-    required: true,
-    validation: () => {
-      return true;
-    },
-  },
-  {
-    text: "Answer with 'Yes' or 'No'",
-    type: "choice",
-    required: true,
-    options: ["Yes", "No", "Maybe", "I don't know"],
-    validation: () => {
-      return true;
-    },
-  },
-  {
-    text: "Would you like assistance?",
-    type: "choice",
-    required: true,
+    text: "Do you use QR payments?",
+    multiple: false,
     options: ["Yes", "No"],
-    validation: () => {
-      return true;
-    },
+    answer: null,
+  },
+  {
+    text: "Which of the following QR payment methods do you use?",
+    options: ["PayNow", "GrabPay", "Singtel Dash", "FavePay"],
+    multiple: true,
+    answer: [],
+  },
+  {
+    text: "Would you like a service to setup QR payments for you?",
+    options: ["Yes", "No"],
+    multiple: false,
+    answer: null,
+  },
+  {
+    text: "Common challenges with QR payment?",
+    options: [
+      "Technical challenges in QR app.",
+      "Connectivity issues affecting QR app.",
+      "Security concerns in QR app.",
+      "Integration challenges with accounting systems.",
+      "Transaction fees for QR payments.",
+    ],
+    multiple: true,
+    answer: [],
   },
 ];
 
 const Survey = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [answers, setAnswers] = useState([
-    "John Doe",
-    "21",
-    "Apples",
-    "Apples",
-    "Yes",
-    "Yes",
-  ]);
+  const [questions, setQuestions] = useState(_questions);
 
   const handlePrevQuestion = () => {
     setCurrentQuestion(currentQuestion - 1);
   };
 
   const handleNextQuestion = () => {
+    if (questions[currentQuestion].answer === null) {
+      return;
+    }
+
+    if (currentQuestion === 0 && questions[currentQuestion].answer === "No") {
+      setCurrentQuestion(currentQuestion + 2);
+      return;
+    }
+
+    if (currentQuestion === 1 && questions[0].answer === "Yes") {
+      setCurrentQuestion(currentQuestion + 2);
+      return;
+    }
+
     setCurrentQuestion(currentQuestion + 1);
   };
 
-  const handleAnswer = (event) => {
-    console.log(event);
-    let value;
-    if (typeof event === "string") {
-      console.log("string");
-      value = event;
-    } else {
-      value = event.target.value;
+  const handleAnswer = (question, answer) => {
+    if (question.multiple) {
+      if (question.answer.includes(answer)) {
+        question.answer = question.answer.filter((ans) => ans !== answer);
+      } else {
+        question.answer.push(answer);
+      }
     }
-    console.log(value, "VALUE?");
-    const newAnswers = [...answers];
-    newAnswers[currentQuestion] = value;
-    setAnswers(newAnswers);
+
+    if (!question.multiple) {
+      question.answer = answer;
+    }
+    setQuestions([...questions]);
   };
 
   const handleSubmit = () => {
     // Handle submission logic here
-    console.log(answers);
+    console.log(questions);
   };
 
-  const renderQuestion = (question, index) => {
-    switch (question.type) {
-      case "text":
-        return (
-          <div key={index}>
-            <label className="my-5 block">{question.text}</label>
-            <input
-              className="block border border-gray-300 p-4 rounded w-full"
-              type="text"
-              value={answers[index] || ""}
-              onChange={handleAnswer}
-              required={question.required}
-            />
+  const renderQuestion = (question) => {
+    if (question.multiple) {
+      return (
+        <div>
+          <h1 className="text-2xl font-bold">{question.text}</h1>
+          <div className="flex flex-col gap-5 my-5">
+            {question.options.map((option) => (
+              <div
+                key={option}
+                onClick={() => handleAnswer(question, option)}
+                className={`w-full border px-10 py-5 text-center font-semibold cursor-pointer rounded-md text-lg hover:bg-blue-400 hover:text-slate-100
+                ${
+                  question.answer.includes(option)
+                    ? "bg-blue-400 text-slate-100"
+                    : "bg-slate-100 text-blue-400"
+                }`}
+              >
+                {option}
+              </div>
+            ))}
           </div>
-        );
-
-      case "multiple_choices":
-        return (
-          <div key={index}>
-            <label className="my-5 block">{question.text}</label>
-            <RadioGroup
-              value={
-                Array.isArray(answers[index])
-                  ? answers[index].join("\n")
-                  : answers[index]
-              }
-              onChange={handleAnswer}
-            >
-              {question.options.map((option, optionIndex) => (
-                <RadioGroup.Option key={optionIndex} value={option}>
-                  {({ checked }) => (
-                    <div
-                      className={`${
-                        checked ? "bg-blue-500" : "bg-white"
-                      } border border-gray-300 p-4 rounded flex justify-between items-center`}
-                    >
-                      <div className="flex items-center">
-                        <div className="text-sm">
-                          <RadioGroup.Label
-                            as="p"
-                            className={`font-medium  ${
-                              checked ? "text-white" : "text-gray-900"
-                            }`}
-                          >
-                            {option}
-                          </RadioGroup.Label>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </RadioGroup.Option>
-              ))}
-            </RadioGroup>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1 className="text-2xl font-bold">{question.text}</h1>
+          <div className="flex flex-col gap-5 my-5">
+            {question.options.map((option) => (
+              <div
+                key={option}
+                onClick={() => handleAnswer(question, option)}
+                className={`w-full border px-10 py-5 text-center font-semibold cursor-pointer rounded-md text-lg hover:bg-blue-400 hover:text-slate-100
+                ${
+                  question.answer === option
+                    ? "bg-blue-400 text-slate-100"
+                    : "bg-slate-100 text-blue-400"
+                }`}
+              >
+                {option}
+              </div>
+            ))}
           </div>
-        );
-
-      case "choice":
-        return (
-          <div key={index}>
-            <label className="my-5 block">{question.text}</label>
-            <RadioGroup
-              value={
-                Array.isArray(answers[index])
-                  ? answers[index].join("\n")
-                  : answers[index]
-              }
-              onChange={handleAnswer}
-            >
-              {question.options.map((option, optionIndex) => (
-                <RadioGroup.Option key={optionIndex} value={option}>
-                  {({ checked }) => (
-                    <div
-                      className={`${
-                        checked ? "bg-blue-500" : "bg-white"
-                      } border border-gray-300 p-4 rounded flex justify-between items-center`}
-                    >
-                      <div className="flex items-center">
-                        <div className="text-sm">
-                          <RadioGroup.Label
-                            as="p"
-                            className={`font-medium  ${
-                              checked ? "text-white" : "text-gray-900"
-                            }`}
-                          >
-                            {option}
-                          </RadioGroup.Label>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </RadioGroup.Option>
-              ))}
-            </RadioGroup>
-          </div>
-        );
-
-      default:
-        return null;
+        </div>
+      );
     }
   };
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen flex-col gap-20">
+    <div className="flex justify-center items-center min-h-screen flex-col gap-20 container px-10">
       <div className="w-min-[500px]">
-        {renderQuestion(questions[currentQuestion], currentQuestion)}
+        {renderQuestion(questions[currentQuestion])}
       </div>
 
       <div className="flex justify-between mt-4 gap-20">
-        {currentQuestion > 0 ? (
+        {/* {currentQuestion > 0 ? (
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={handlePrevQuestion}
@@ -214,7 +145,7 @@ const Survey = () => {
           </button>
         ) : (
           <div></div>
-        )}
+        )} */}
 
         {currentQuestion < questions.length - 1 && (
           <button
