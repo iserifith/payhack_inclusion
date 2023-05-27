23b55dd99fb3def9e844dbcfd1690b697a966d1b
DD <aizuddin.hamzah@timeteccloud.com>
2023-05-27 22:43:55 +0800

    update dashboard

--- src/components/ResponseRateChart.js
+++ src/components/ResponseRateChart.js
@@ -1,7 +1,67 @@
-import React from "react";
+
+import React, { useState, useEffect } from 'react';
+import 'chart.js/auto';
+import { Pie } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const ResponseRateChart = () => {
-  return <div>ResponseRateChart</div>;
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    setChartData({
+        labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+        datasets: [
+            {
+                label: 'Sales $',
+                data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgb(53, 162, 235, 0.4',
+              }, 
+        ]
+    })
+    setChartOptions({
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Daily Revenue'
+            }
+        },
+        maintainAspectRatio: false,
+        responsive: true
+    })
+  }, [])
+
+  return (
+    <>
+      <div className='w-full md:col-span-1 relative lg:h-[50vh] h-[50vh] m-auto p-4 border rounded-lg bg-white'>
+        <Pie data={chartData} options={chartOptions} />
+      </div>
+    </>
+  );
 };
 
 export default ResponseRateChart;
