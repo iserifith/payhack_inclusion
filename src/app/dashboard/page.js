16cca80449d8b5904b520b4549622b887460938c
xrn <seriputera2@gmail.com>
2023-05-27 16:45:47 +0800

    --

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,49 @@
+"use client";
+import { getSmeInfo } from "@/db";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 const Page = () => {
-  return <div>Page</div>;
+  // totalSmeAvailable: "2000",
+  // totalSmeWithContact: "800",
+  // totalInquiriesSent: "100",
+  const smesInfo = getSmeInfo();
+  return (
+    <div>
+      <Pie
+        data={{
+          labels: [
+            "Total SME Available",
+            "Total SME with Contact",
+            "Total Inquiries Sent",
+          ],
+          datasets: [
+            {
+              label: "SME Info",
+              data: [
+                smesInfo.totalSmeAvailable,
+                smesInfo.totalSmeWithContact,
+                smesInfo.totalInquiriesSent,
+              ],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+      />
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/db.js
@@ -0,0 +1,55 @@
+const sme_info = {
+  totalSmeAvailable: "2000",
+  totalSmeWithContact: "800",
+  totalInquiriesSent: "100",
+};
+
+const response_rate = {
+  respondedSme: "2000",
+  respondedSmePerc: "30%",
+};
+
+const qr_usage = {
+  respondedUseQR: "1000",
+  respondedUseQRPerc: "50%",
+};
+
+const qr_dist = {
+  DuitNow: "3000",
+  TNG: "1400",
+  MAE: "200",
+};
+
+const prospect = {
+  yesTotal: 10000,
+  yesPerc: "20%",
+};
+
+const challenges = {
+  "do not have app": "3000",
+  tedious: "3230",
+};
+
+export const getSmeInfo = () => {
+  return sme_info;
+};
+
+export const getResponse_rate = () => {
+  return response_rate;
+};
+
+export const getQRUsage = () => {
+  return qr_usage;
+};
+
+export const getQRDist = () => {
+  return qr_dist;
+};
+
+export const getProspect = () => {
+  return prospect;
+};
+
+export const getChallenges = () => {
+  return challenges;
+};
