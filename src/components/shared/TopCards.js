a341fd7ab01013aa77e5a5841d169ec57259bce3
hafyze <hafizmatqwer@gmail.com>
2023-05-28 09:55:19 +0800

    Topbar Update

--- src/components/shared/TopCards.js
+++ src/components/shared/TopCards.js
@@ -47,8 +47,8 @@ const TopCards = () => {
         </div>
         <div className='lg:col-span-1 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
             <div className='flex flex-col w-full pb-4'>
-                <p className='text-2xl font-bold'>2000</p>
-                <p className='text-gray-600'>Responded SME</p>
+                <p className='text-2xl font-bold'>{smedata.respondedSmePerc}</p>
+                <p className='text-gray-600'>Feedback Received</p>
             </div>
             <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
                 <span className='text-green-700 text-lg'>30%</span>
@@ -63,6 +63,7 @@ const TopCards = () => {
                 <span className='text-green-700 text-lg'>50%</span>
             </p>
         </div>
+        
     </div>
   )
 }
