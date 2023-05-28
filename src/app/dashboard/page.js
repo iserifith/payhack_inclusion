f46757352df08a614ed8fcbd5c16c089e920f782
DD <aizuddin.hamzah@timeteccloud.com>
2023-05-28 09:19:39 +0800

    push

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -11,26 +11,13 @@ import { getResponseRate } from "@/db";
 import { useEffect, useState } from "react";
 
 const Page = () => {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getResponseRate();
-      setData(result);
-      setLoading(false)
-    };
-    fetchData();
-  }, []);
 
-  if (loading) {
-    return null
-  }
 
   return (
     <>
       <main className="bg-gray-100 min-h-screen">
         <Header />
-        <TopCards data = {data}/>
+        <TopCards />
         <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
           <ResponseRateChart />
           <ResponseRateChart  />
